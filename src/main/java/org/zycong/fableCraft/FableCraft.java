3ad585786c3de720c24bb962f3d2b934a4047bf1
zycong <lmdonders@outlook.com>
2025-03-30 11:49:36 +0200

    Initial Commit

---
+++ src/main/java/org/zycong/fableCraft/FableCraft.java
@@ -0,0 +1,17 @@
+package org.zycong.fableCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FableCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
