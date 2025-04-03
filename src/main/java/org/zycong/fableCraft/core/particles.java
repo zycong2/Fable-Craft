06dd7e795dfd6bf827419926d6e8ce9922a5b22b
timsterNED <t245538@gmail.com>
2025-04-03 17:53:11 +0200

    a start of particles

--- src/main/java/org/zycong/fableCraft/core/particles.java
+++ src/main/java/org/zycong/fableCraft/core/particles.java
@@ -1,8 +1,18 @@
-package org.zycong.fableCraft.core.particles;
+package org.zycong.fableCraft.core;
+
+import com.destroystokyo.paper.ParticleBuilder;
+import org.bukkit.Particle;
+
+import javax.xml.stream.Location;
 
 public class particles {
 
-  public void makeParticle(Location loc, int amount, string particle, int a, int r, int g, int b){
-    
+  public void makeParticle(Location loc, int amount, String particle, int a, int r, int g, int b){
+    ParticleBuilder
+    .color((int) a, (int) r,(int)  g,(int)  b)
+            .locatoin(loc)
+            .count(100)
+            .particle(Particle.valueOf(particle))
+            .spawn();;
   }
 }
