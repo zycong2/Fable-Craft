828eb5485c9e4d9e558bb424a53b14e4ca52adc0
zycong <lmdonders@outlook.com>
2025-04-11 15:13:25 +0200

    added some particle library

--- src/main/java/io/RPGCraft/FableCraft/core/particles.java
+++ src/main/java/io/RPGCraft/FableCraft/core/particles.java
@@ -1,18 +1,42 @@
-/*package org.zycong.fableCraft.core;
+package io.RPGCraft.FableCraft.core;
 
-import com.destroystokyo.paper.ParticleBuilder;
+import org.bukkit.Location;
 import org.bukkit.Particle;
-
-import javax.xml.stream.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
 public class particles {
 
-  public void makeParticle(Location loc, int amount, String particle, int a, int r, int g, int b){
-    ParticleBuilder
-    .color((int) a, (int) r,(int)  g,(int)  b)
-            .locatoin(loc)
-            .count(100)
-            .particle(Particle.valueOf(particle))
-            .spawn();;
+  public void makeParticle(Location loc, int amount, String particle) {
+    loc.getWorld().spawnParticle(Particle.valueOf(particle), loc, amount);
   }
-}*/
+
+  public void particleLine(Location loc, int amount, String particle, int length){
+    double t = 0;
+    int loops = amount/length;
+    for (int i = 0; i < length; i+=loops){
+      Vector direction = loc.getDirection().normalize();
+      double x = direction.getX() * i;
+      double y = direction.getY() * i + 1.5;
+      double z = direction.getZ() * i;
+      loc.add(x, y, z);
+      makeParticle(loc, 1, particle);
+      loc.subtract(x,y,z);
+    }
+  }
+  public void particleCyl(Location loc, int amount, String particle, int diameter, boolean hollow){
+    for(double phi=0; phi<=Math.PI; phi+=Math.PI/15) {
+      for(double theta=0; theta<=2*Math.PI; theta+=Math.PI/30) {
+        double r = 1.5;
+        double x = r*Math.cos(theta)*Math.sin(phi);
+        double y = r*Math.cos(phi) + 1.5;
+        double z = r*Math.sin(theta)*Math.sin(phi);
+
+        loc.add(x,y,z);
+        makeParticle(loc, amount, particle);
+        loc.subtract(x, y, z);
+      }
+    }
+  }
+}
