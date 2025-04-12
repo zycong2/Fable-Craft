fec551f4c4f9f8b17a5dd8bbb0174ff186a8be70
Tonnam101 <Tonnam122012@gmail.com>
2025-04-12 07:08:39 +0700

    Added citizen and luckperm api

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,18 @@ java {
   toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
+repositories {
+  mavenCentral()
+}
 
 dependencies {
   paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
   implementation("net.kyori:adventure-text-minimessage:4.19.0")
   compileOnly("org.projectlombok:lombok:1.18.30")
+  compileOnly("net.luckperms:api:5.4")
+  compileOnly("net.citizensnpcs:citizens-main:2.0.35-SNAPSHOT") {
+    exclude(group = "*", module = "*")
+  }
   annotationProcessor ("org.projectlombok:lombok:1.18.30")
 }
 
