b5eb27c90f1ba32828b2ea83c170e5f589aaf4e3
Tonnam101 <Tonnam122012@gmail.com>
2025-04-20 20:11:54 +0700

    quest

--- src/main/java/io/RPGCraft/FableCraft/DataTypes/Quests/Helper/Objective.java
+++ src/main/java/io/RPGCraft/FableCraft/DataTypes/Quests/Helper/Objective.java
@@ -1,45 +1,39 @@
 package io.RPGCraft.FableCraft.DataTypes.Quests.Helper;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+
 public class Objective {
+  private String name;
+  private int count;
+  private ObjectiveTypes type;
 
-  private int Count;
-  private int NPCID;
-  private String Name;
-  private ObjectiveTypes types;
+  // Dynamic data based on type
+  private Entity mobToKill;
+  private ItemStack itemToObtain;
+  private Location targetLocation;
+  private int npcId;
 
-  public ObjectiveTypes getTypes() {
-    return types;
-  }
+  // Getters and Setters
+  public Entity getMobToKill() { return mobToKill; }
+  public Objective mobToKill(Entity type) { this.mobToKill = type; return this; }
 
-  public Objective types(ObjectiveTypes types) {
-    this.types = types;
-    return this;
-  }
+  public ItemStack getItemToObtain() { return itemToObtain; }
+  public Objective itemToObtain(ItemStack item) { this.itemToObtain = item; return this; }
 
-  public String getName() {
-    return Name;
-  }
+  public Location getTargetLocation() { return targetLocation; }
+  public Objective targetLocation(Location loc) { this.targetLocation = loc; return this; }
 
-  public Objective name(String name) {
-    Name = name;
-    return this;
-  }
+  public int getNpcId() { return npcId; }
+  public Objective npcId(int id) { this.npcId = id; return this; }
 
-  public int getNPCID() {
-    return NPCID;
-  }
+  public ObjectiveTypes getTypes() { return type; }
+  public Objective types(ObjectiveTypes type) { this.type = type; return this; }
 
-  public Objective NPCID(int ID) {
-    this.NPCID = ID;
-    return this;
-  }
+  public int getCount() { return count; }
+  public Objective count(int count) { this.count = count; return this; }
 
-  public int getCount() {
-    return Count;
-  }
-
-  public Objective count(int count) {
-    Count = count;
-    return this;
-  }
+  public String getName() { return name; }
+  public Objective name(String name) { this.name = name; return this; }
 }
