9fa30965690968bf5f7ff066e91ffdc5734a8574
Tonnam101 <Tonnam122012@gmail.com>
2025-04-25 19:46:11 +0700

    base gemstones

--- src/main/java/io/RPGCraft/FableCraft/Utils/Gemstone/GemStone.java
+++ src/main/java/io/RPGCraft/FableCraft/Utils/Gemstone/GemStone.java
@@ -1,4 +1,32 @@
 package io.RPGCraft.FableCraft.Utils.Gemstone;
 
+import io.RPGCraft.FableCraft.Utils.Placeholders.PlaceholderTypes.PlayerPlaceholders;
+import io.RPGCraft.FableCraft.Utils.Placeholders.PlaceholderUtils.Placeholder;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GemStone {
+  private static final Map<String, Method> Redstone = new HashMap<>();
+  private static final Map<String, Method> Bluestone = new HashMap<>();
+  private static final Map<String, Method> Pinkstone = new HashMap<>();
+
+  public GemStone() {
+    registerPlaceholders(Gemstones.class);
+  }
+
+  public static <T> void registerPlaceholders(Class<?> c) {
+    for (Method m : c.getDeclaredMethods()) {
+      if (m.isAnnotationPresent(GemstoneAnnotation.class)) {
+        GemstoneAnnotation ann = m.getAnnotation(GemstoneAnnotation.class);
+        switch (ann.type()){
+          case RED -> Redstone.put(ann.name(), m);
+          case BLUE -> Bluestone.put(ann.name(), m);
+          case PINK -> Pinkstone.put(ann.name(), m);
+        }
+      }
+    }
+  }
+
 }
---
+++ src/main/java/io/RPGCraft/FableCraft/Utils/Gemstone/Gemstones.java
@@ -0,0 +1,10 @@
+package io.RPGCraft.FableCraft.Utils.Gemstone;
+
+public class Gemstones {
+
+  @GemstoneAnnotation(name = "Bloody Hand", type = GemstoneType.RED)
+  public static String bloodyHand() {
+    return "Bloody Hand";
+  }
+
+}
