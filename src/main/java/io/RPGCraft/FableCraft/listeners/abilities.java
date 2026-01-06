5af788f859fdcefb21f0071d1f492a5be2166c75
zycong <lmdonders@outlook.com>
2026-01-06 14:04:46 +0100

    Update abilities.java

--- src/main/java/io/RPGCraft/FableCraft/listeners/abilities.java
+++ src/main/java/io/RPGCraft/FableCraft/listeners/abilities.java
@@ -2,65 +2,103 @@ package io.RPGCraft.FableCraft.listeners;
 
 import io.RPGCraft.FableCraft.core.Helpers.PDCHelper;
 import io.RPGCraft.FableCraft.core.YAML.yamlGetter;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
-import java.util.List;
+import java.time.Instant;
 
 public class abilities implements Listener {
+  Map<String, long> activeCooldown = new HashMap<>();
   @EventHandler
   void onDamage(EntityDamageByEntityEvent e){
     if (e.getDamager() instanceof Player p){
-      // uhhhhhhhh I'm pretty sure there's more than one parameter
-      // checkAbilities(p); It's giving me some error
+      checkAbilities(p);
     }
   }
 
-  /**
-   * Explain what this does here
-   *
-   * @param p Explain what this parameter is here
-   * @param event Explain what this parameter is here
-   * @param victim Explain what this parameter is here
-   */
   void checkAbilities(Player p, String event, LivingEntity victim){
     for (ItemStack item : p.getInventory().getContents()){
       Object customItem = PDCHelper.getItemPDC("customItemName", item);
       if (customItem == null) { continue; }
       if (yamlGetter.getPathInDB("itemDB", customItem + ".abilities") == null) {continue;}
-      List<String> abilities = yamlGetter.getNodes("itemDB", "customItem.abilities").stream().toList();
+      List<String> abilities = yamlManager.getInstace().getNodes("customItem + ".abilities"");
       for (String s : abilities){
-        /* I'm pretty sure this is wrong like shouldn't there be like rightclick left click also
-        * I'm pretty sure you mean to switch S and not whatever that is I just fixed some errors
-        * and it probably won't work but if it does it's a miracle
-         */
-        switch (yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".type").toString()) {
+        switch (yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".type")) {
           case "potion" -> {
-            // Better replace this with something else before PotionEffectType.getByName broke :) (I hate my life)
-            PotionEffectType effectType = PotionEffectType.getByName(yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".type").toString());
-            // WTF IS THIS BRO IT BREAK AND IT KEEP BREAKING AHHHHHH
-            //PotionEffect effect = PotionEffectType.getByName(effectType.toString().toUpperCase());
-
-            // These two lines are also probably unstable replace it with something else idk
-            int duration = Integer.parseInt(yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".duration").toString());
-            int amp = Integer.parseInt(yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".level").toString());
-            victim.addPotionEffect(new PotionEffect(effectType, duration, amp), true);
+            PotionEffectType effectType yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".type");
+            PotionEffect effect = PotionEffectType.getByName(effectType.toUpperCase());
+            int duration = yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".duration");
+            int amp = yamlGetter.getPathInDB("itemDB", customItem + ".abilities." + s + ".level");
+            victim.addPotionEffect(new PotionEffect(effect, duration, amp), true);
           }
-          case "" -> {
-
+          case "lighting" -> {
+            p.getWorld().strikeLightningEffect(victim.getLocation());
           }
-          case null -> {
-            // idk what to put here whoops
-          }
-          default -> throw new IllegalStateException("Unexpected value: " + s);
         }
       }
     }
   }
+
+  public void executeAbility(String name){
+    if (getPathInDB("abilities", name + ".id") == null) {
+      Bukkit.getLogger().warning("Coudnt find ability: " + name);
+      return;
+    }
+    if (getPathInDB("abilities", name + ".cooldown") != null){
+      if (activeCooldown.get("name") != null){
+        Instant now = Instant.now();
+        long currentTimeInSeconds = now.getEpochSecond();
+        long time = activeCooldown.get("name");
+        if (Math.Abs(time  - currentTimeInSeconds) > Integer.valueOf(getPathInDB("abilities", name + ".cooldown"))  ) {
+          return;
+        }
+        activeCooldown.remove(name);
+      } else {
+        Instant now = Instant.now();
+        long currentTimeInSeconds = now.getEpochSecond();
+        activeCooldown.put(name, currentTimeInSeconds);
+      }
+    }
+
+    
+
+    
+  }
 }
+
+
+/*ability:
+  id: lightning_strike
+  cooldown: 12s
+
+  trigger:
+    type: right_click
+    item: STICK
+
+  actions:
+    - ray_trace:
+        max_distance: 30
+
+        on_hit:
+          target: entity
+          actions:
+            - if:
+                condition:
+                  target.is_player == true
+                then:
+                  - damage:
+                      amount: 10
+                else:
+                  - damage:
+                      amount: 15
+
+            - particle:
+                type: ELECTRIC_SPARK
+                count: 40
+
+            - sound:
+                type: ENTITY_LIGHTNING_BOLT_THUNDER
+*/
