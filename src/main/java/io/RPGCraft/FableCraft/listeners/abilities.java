6cc81641500a255fe87010aa143f6f4335458e06
luc <lmdonders@outlook.com>
2026-01-08 16:19:49 +0100

    if you touch this file im gone touch you or smt

--- src/main/java/io/RPGCraft/FableCraft/listeners/abilities.java
+++ src/main/java/io/RPGCraft/FableCraft/listeners/abilities.java
@@ -2,6 +2,7 @@ package io.RPGCraft.FableCraft.listeners;
 
 import io.RPGCraft.FableCraft.core.Helpers.PDCHelper;
 import io.RPGCraft.FableCraft.core.YAML.yamlGetter;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,18 +18,13 @@ import java.util.List;
 import java.util.Map;
 
 import static io.RPGCraft.FableCraft.core.YAML.yamlGetter.getPathInDB;
-import static org.bukkit.Bukkit.getServer;
 
 public class abilities implements Listener {
-
-  // Trust me it does not fucking work are you coding in note pad?
-  Map<String, Long> activeCooldown = new HashMap();
-
+  Map<String,Long> activeCooldown = new HashMap<>();
   @EventHandler
   void onDamage(EntityDamageByEntityEvent e){
     if (e.getDamager() instanceof Player p){
-      // uhhhhhhhh I'm pretty sure there's more than one parameter
-      // checkAbilities(p); It's giving me some error
+      checkAbilities(p, "attack", (LivingEntity) e.getEntity());
     }
   }
 
@@ -37,30 +33,19 @@ public class abilities implements Listener {
       Object customItem = PDCHelper.getItemPDC("customItemName", item);
       if (customItem == null) { continue; }
       if (getPathInDB("itemDB", customItem + ".abilities") == null) {continue;}
-      List<String> abilities = yamlGetter.getNodes("itemDB", "customItem.abilities").stream().toList();
-      for (String s : abilities){
-        /* I'm pretty sure this is wrong like shouldn't there be like rightclick left click also
-        * I'm pretty sure you mean to switch S and not whatever that is I just fixed some errors
-        * and it probably won't work but if it does it's a miracle
-         */
-        // IT DOES NOT WORK DON'T USE NOTEPAD PLEASE
+      List<Object> abilities = yamlGetter.getAllNodesInDB("itemDB", "customItem" + ".abilities");
+      for (Object s : abilities){
         switch (getPathInDB("itemDB", customItem + ".abilities." + s + ".type").toString()) {
           case "potion" -> {
-            // Better replace this with something else before PotionEffectType.getByName broke :) (I hate my life)
-            PotionEffectType effectType = PotionEffectType.getByName(getPathInDB("itemDB", customItem + ".abilities." + s + ".type").toString());
-            // WTF IS THIS BRO IT BREAK AND IT KEEP BREAKING AHHHHHH
-            //PotionEffect effect = PotionEffectType.getByName(effectType.toString().toUpperCase());
-
-            // These two lines are also probably unstable replace it with something else idk
+            String effectType = getPathInDB("itemDB", customItem + ".abilities." + s + ".type").toString();
+            PotionEffectType effect = PotionEffectType.getByName(effectType.toUpperCase());
             int duration = Integer.parseInt(getPathInDB("itemDB", customItem + ".abilities." + s + ".duration").toString());
             int amp = Integer.parseInt(getPathInDB("itemDB", customItem + ".abilities." + s + ".level").toString());
-            victim.addPotionEffect(new PotionEffect(effectType, duration, amp), true);
+            victim.addPotionEffect(new PotionEffect(effect, duration, amp), true);
           }
           case "lighting" -> {
             p.getWorld().strikeLightningEffect(victim.getLocation());
           }
-
-          default -> throw new IllegalStateException("Unexpected value: " + s);
         }
       }
     }
@@ -68,7 +53,7 @@ public class abilities implements Listener {
 
   public void executeAbility(String name){
     if (getPathInDB("abilities", name + ".id") == null) {
-      getServer().getLogger().warning("Coudnt find ability: " + name);
+      Bukkit.getLogger().warning("Coudnt find ability: " + name);
       return;
     }
     if (getPathInDB("abilities", name + ".cooldown") != null){
@@ -87,9 +72,9 @@ public class abilities implements Listener {
       }
     }
 
-    
 
-    
+
+
   }
 }
 
